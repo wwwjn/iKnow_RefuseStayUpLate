3b356009d86e190a9598a7a684b24e9c8681e776
libilu61BB <32815142+libilu61BB@users.noreply.github.com>
2018-11-27 23:51:20 +0800

    add setLabel page and setNotice page

--- app/src/main/java/com/example/phone_test/MainActivity.java
+++ app/src/main/java/com/example/phone_test/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.phone_test;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+    //进入“开发者信息”页面
+    public void getDesignerInfo(View view)
+    {
+        Intent intent = new Intent(this, OurInfo.class);
+        startActivity(intent);
+    }
+
+    //进入“反馈信息”页面
+    public void gotoFeedback(View view)
+    {
+        Intent intent = new Intent(this, Feedback.class);
+        startActivity(intent);
+    }
+
+    //返回上一页面
+    public void goBack(View view)
+    {
+        finish();
+    }
+
+
+
+
+
 }
+
+
